21d5ffb61c99fb53781d1a743a8a868ce2478375
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2024-10-07 09:35:56 +0200

    klar

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,25 @@ public class Main {
         System.out.println("vad heter din tama?");
         tama.name = sc.nextLine();
         while(mytama.getAlive()) {
-            int val = menuChoice(new String[]{"Lär den nya ord", "Få den att säga något", "Gör ingenting", "Mata den"});
+            int val = menuChoice(new String[]{"Lär den nya ord", "Få den att säga något", "Gör ingenting", "Mata den", "Statistik om tama"});
             if (val == 1) {
                 System.out.println("Vilket ord vill du lära den?");
                 mytama.teach(sc.nextLine());
+                mytama.tick();
             } else if (val == 2) {
                 mytama.speak();
+                mytama.tick();
             } else if (val == 3) {
-                break;
+                mytama.tick();
             } else if (val == 4){
                 mytama.feed();
+                mytama.tick();
+            } else if (val == 5){
+                mytama.printStats();
+                mytama.tick();
+            }
+            if(mytama.getAlive() == false) {
+                System.out.println(mytama.name + " dog, REST IN PEACE :SKULL:");
             }
         }
     }
