5c006642e12a103527a63591e14c51cc03adc0ab
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2024-10-15 10:08:55 +0200

    bra kod

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,20 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Tama> tamaList = new ArrayList<>();
         while (true) {
-            String name;
             for(int i = 0; i < tamaList.size(); i++){
                 System.out.println(i+1 + ". Ladda " + tamaList.get(i).name);
             }
             System.out.println(tamaList.size()+1 + ". Skapa en ny tama");
-            var val1 = sc.nextInt();
+            var val = sc.nextInt();
             sc.nextLine();
-            if (val1 == tamaList.size()+1) {
+            if (val == tamaList.size()+1) {
                 System.out.println("Vad ska din nya tama heta?");
-                name = sc.nextLine();
+                String name = sc.nextLine();
                 Tama newTama = new Tama(name);
                 tamaList.add(newTama);
                 newTama.gameLoop();
-            } else if (val1 == 1) {
-                tamaList.get(0).gameLoop();
-            } else if (val1 == 2) {
-                tamaList.get(1).gameLoop();
-            } else if (val1 == 3) {
-                tamaList.get(2).gameLoop();
-            }else if (val1 == 4) {
-                tamaList.get(3).gameLoop();
-            } else if (val1 == 5) {
-                tamaList.get(4).gameLoop();
+            } else {
+                tamaList.get(val-1).gameLoop();
             }
         }
     }
